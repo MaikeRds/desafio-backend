b7ea3d81aa8e24b0a0108f6abd35a6d28e6a837f
Maike Rodrigues <maikerodrigues@cialne.com.br>
2020-11-05 09:24:14 -0300

    Configuração prettier e adicionado typeorm e pg.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    createDefaultProgram: true,
+    tsconfigRootDir: './',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -20,5 +24,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
   },
 };
